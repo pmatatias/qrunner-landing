1935ccea8b4e7682ed9df71fa8ff883838be454c
pmatatias <matatias97@gmail.com>
2023-12-24 23:56:35 +0700

    chore update:

--- src/components/sections/feature.tsx
+++ src/components/sections/feature.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import * as styles from "./styles.module.scss";
 import Container from "../container";
+import BaseButton from "../basebutton";
+import Icon from "../icon";
+// import Arorw from "../../images/arrow_right.svg";
 const Features = (props: any) => {
   return (
     <Container>
@@ -10,11 +13,24 @@ const Features = (props: any) => {
       </h1>
       <p className={styles.mainDesc}>
         Any repetitive operational tasks that require engineers' manual effort
-        like running a self-made script, running <br className={styles.breakLine}></br>SQL queries, data patching for
+        like running a self-made script, running{" "}
+        <br className={styles.breakLine}></br>SQL queries, data patching for
         manual ops, can be turned into a self-serve tool that can be handed over
-        to the <br className={styles.breakLine}></br>operational team, PMs, or anyone who doesn't have any experience
-        with engineering.
+        to the <br className={styles.breakLine}></br>operational team, PMs, or
+        anyone who doesn't have any experience with engineering.
       </p>
+      <div className={styles.btnrow}>
+        <BaseButton label="Start your free trial" />
+        <a>Plans and pricing</a>
+        <Icon
+        src={'../../images/arrow_right.svg'}
+        alt="Custom Icon"
+        width="50"
+        height="50"
+        className="custom-icon"
+        style={{ color: 'red' }}
+      />
+      </div>
     </Container>
   );
 };
